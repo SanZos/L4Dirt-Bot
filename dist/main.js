560e03a9ddbc66c276c7304165a9f4a1bdaaf1a7
iuzziel <iuzziel.abrudavah@outlook.com>
2020-11-05 01:49:23 +0100

    Add npmignore

--- dist/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.bot = void 0;
-const bot_1 = require("./bot");
-const server_1 = require("./web/server");
-server_1.server.listen(process.env.PORT || 8080);
-exports.bot = new bot_1.Left4DirtBot();
-exports.bot.login();
